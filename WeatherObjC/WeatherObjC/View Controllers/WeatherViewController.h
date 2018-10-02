0c360ad41f65ee5928167dbe441315ecd565d7a6
Conner Alegre <cga0@pm.me>
2018-10-02 13:13:49 -0600

    add base files (stubs) for main vc and view cell

---
+++ WeatherObjC/WeatherObjC/View Controllers/WeatherViewController.h
@@ -0,0 +1,17 @@
+//
+//  WeatherViewController.h
+//  WeatherObjC
+//
+//  Created by Conner on 10/2/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WeatherViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
