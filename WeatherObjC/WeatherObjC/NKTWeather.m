1d84dd0e38b4f81a59ee06a9fd779cba003bc7ff
OmegaWulf <nikcxz@gmail.com>
2019-01-22 17:09:14 -0500

    Model

---
+++ WeatherObjC/WeatherObjC/NKTWeather.m
@@ -0,0 +1,13 @@
+//
+//  NKTWeather.m
+//  WeatherObjC
+//
+//  Created by Nikita Thomas on 1/22/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+#import "NKTWeather.h"
+
+@implementation NKTWeather
+
+@end
