3dc85c7761de59a1d4a00a163d620d3cb70ec6f3
yzhukovs <yzhukovs@bumnetworks.com>
2019-01-22 17:03:29 -0500

    storyboard setup

---
+++ WeatherObjC/WeatherObjC/YZIWeatherViewController.h
@@ -0,0 +1,18 @@
+//
+//  YZIWeatherViewController.h
+//  WeatherObjC
+//
+//  Created by Yvette Zhukovsky on 1/22/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YZIWeatherViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UISearchBar *searchZip;
+
+@end
+
+NS_ASSUME_NONNULL_END
