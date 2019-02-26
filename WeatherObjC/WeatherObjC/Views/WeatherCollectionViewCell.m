54ac92bee3dce9b6330d0d77db875126a9447c3f
IvanCaldwell <grumpy.caldwell@gmail.com>
2019-02-26 12:40:12 -0800

    Storyboard Setup

---
+++ WeatherObjC/WeatherObjC/Views/WeatherCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  WeatherCollectionViewCell.m
+//  WeatherObjC
+//
+//  Created by Ivan Caldwell on 2/26/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+#import "WeatherCollectionViewCell.h"
+
+@implementation WeatherCollectionViewCell
+
+@end
