29e648b46bf865bd903d5ab37839ddeb0b01fa58
Sergey Osipyan <osipyansergey@gmail.com>
2019-02-26 14:25:03 -0800

    Part 1 done

---
+++ WeatherObjC/WeatherObjC/View Cell/OSIWeatherCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  OSIWeatherCollectionViewCell.m
+//  WeatherObjC
+//
+//  Created by Sergey Osipyan on 2/26/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+#import "OSIWeatherCollectionViewCell.h"
+
+@implementation OSIWeatherCollectionViewCell
+
+@end
