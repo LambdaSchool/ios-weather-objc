cb84ec8103881326b3e7eb6630d35ea159df7a1e
Audrey Welch <audreywelch@Audreys-MacBook-Pro.local>
2019-02-26 21:51:12 -0600

    Model and Model Controller created

---
+++ WeatherObjC/WeatherObjC/Model/ALWModelController.h
@@ -0,0 +1,15 @@
+//
+//  ALWModelController.h
+//  
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ALWModelController : NSObject
+    
+@property (nonatomic, readonly) NSArray *forecasts;
+
+@end
+
+NS_ASSUME_NONNULL_END
