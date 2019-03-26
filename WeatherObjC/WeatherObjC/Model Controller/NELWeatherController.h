a954589c584e18470e01270803013e6b3d32b83b
Nelson Gonzalez <nelglezfl@gmail.com>
2019-03-26 17:11:03 -0400

    Add weather controller header.

---
+++ WeatherObjC/WeatherObjC/Model Controller/NELWeatherController.h
@@ -0,0 +1,22 @@
+//
+//  NELWeatherController.h
+//  WeatherObjC
+//
+//  Created by Nelson Gonzalez on 3/26/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NELWeatherController : NSObject
+
+- (void)getWeatherForCity:(NSString *)cityName completion:(void (^)(NSArray *forecasts, NSError *error))completion;
+
+
+@property NSMutableArray *forecasts;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WeatherObjC/WeatherObjC/Model Controller/NELWeatherController.m
@@ -0,0 +1,13 @@
+//
+//  NELWeatherController.m
+//  WeatherObjC
+//
+//  Created by Nelson Gonzalez on 3/26/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+#import "NELWeatherController.h"
+
+@implementation NELWeatherController
+
+@end
