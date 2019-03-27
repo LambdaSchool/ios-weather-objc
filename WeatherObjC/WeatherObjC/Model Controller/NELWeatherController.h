592803b68c6ba92c3f16c41e35b8793b4eb304c7
Nelson Gonzalez <nelglezfl@gmail.com>
2019-03-27 14:44:14 -0400

    Clean code.

--- WeatherObjC/WeatherObjC/Model Controller/NELWeatherController.h
+++ WeatherObjC/WeatherObjC/Model Controller/NELWeatherController.h
@@ -15,8 +15,6 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)getWeatherForCity:(NSString *)cityName completion:(void (^)(NSArray *forecasts, NSError *error))completion;
 
 
-//@property NSMutableArray *forecasts;
-
 @end
 
 NS_ASSUME_NONNULL_END
