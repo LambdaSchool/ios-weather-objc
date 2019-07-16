1e74a0de99dd676b2d104e4f9014bae8e81258ce
Hayden Hastings <haydenhastings32@gmail.com>
2019-07-16 15:26:24 -0600

    Added fetch method in model controller to get the weather from zip code.

--- WeatherObjC/WeatherObjC/Model Controller/HHWeatherController.m
+++ WeatherObjC/WeatherObjC/Model Controller/HHWeatherController.m
@@ -7,6 +7,7 @@
 //
 
 #import "HHWeatherController.h"
+#import "HHWeather.h"
 
 @implementation HHWeatherController
 
@@ -14,6 +15,51 @@ static NSString * const baseURLString = @"https://api.openweathermap.org/data/2.
 static NSString * const apiKey = @"14cb40aeaa54152e2db5015c9830c260";
 
 - (void)fetchWeatherForCity:(NSString *)cityName completionBlock:(HHWeatherControllerCompletionBlock)completionBlock {
+    NSURL *baseURL = [NSURL URLWithString:baseURLString];
+    NSURLComponents *components = [NSURLComponents componentsWithURL:baseURL resolvingAgainstBaseURL:YES];
+    NSURLQueryItem *city = [NSURLQueryItem queryItemWithName:@"q" value:cityName];
+    NSURLQueryItem *metric = [NSURLQueryItem queryItemWithName:@"units" value:@"metric"];
+    NSURLQueryItem *cnt = [NSURLQueryItem queryItemWithName:@"cnt" value:@"7"];
+    NSURLQueryItem *apiKeyItem = [NSURLQueryItem queryItemWithName:@"appid" value:apiKey];
     
+    [components setQueryItems:@[city, metric, cnt, apiKeyItem]];
+    
+    NSURL *requestURL = components.URL;
+    NSURLRequest *request = [NSURLRequest requestWithURL:requestURL];
+    
+    NSURLSessionDataTask *dataTask = [[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData * data, NSURLResponse * response, NSError * error) {
+        if (error) {
+            NSLog(@"Error fetching data: %@", error);
+            completionBlock(nil, error);
+            return;
+        }
+        
+        if (data) {
+            NSLog(@"No data returned.");
+            completionBlock(nil, [[NSError alloc] init]);
+            return;
+        }
+        
+        NSDictionary *jsonDictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+        
+        if (![jsonDictionary isKindOfClass:[NSDictionary class]]) {
+            NSLog(@"Json wsa not a dictionary");
+            completionBlock(nil, [[NSError alloc] init]);
+            return;
+        }
+        
+        NSString *nameCity = jsonDictionary[@"city"][@"name"];
+        NSArray *forecastDictionary = jsonDictionary[@"list"];
+        
+        NSMutableArray *localWeather = [[NSMutableArray alloc] init];
+        
+        for (NSDictionary *forecastDict in forecastDictionary) {
+            HHWeather *weather = [[HHWeather alloc] initWithCityName:nameCity dictionary:forecastDict];
+            
+            [localWeather addObject:weather];
+        }
+        completionBlock([self forecasts], nil);
+    }];
+    [dataTask resume];
 }
 @end
