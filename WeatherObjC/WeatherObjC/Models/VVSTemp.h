f0a55b59cd8c2743a7ff8eeb7240435a80c8a029
Vici Shaweddy <vshaweddy@gmail.com>
2020-02-02 14:52:11 -0500

    setting up the models

---
+++ WeatherObjC/WeatherObjC/Models/VVSTemp.h
@@ -0,0 +1,21 @@
+//
+//  VVSTemp.h
+//  WeatherObjC
+//
+//  Created by Vici Shaweddy on 2/2/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VVSTemp : NSObject
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary;
+
+@property (nonatomic, readonly) NSDate *date;
+@property (nonatomic, readonly) double temp;
+@property (nonatomic, readonly, copy) NSString *icon;
+
+
+@end
+
